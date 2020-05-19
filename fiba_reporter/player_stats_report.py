e97935f8859f191f1f3bfc1e6bc2b6595b2638fa
linweihao <nypgand1@gmail.com>
2020-05-19 13:41:25 +0800

    add csv format for player stats report

--- fiba_reporter/player_stats_report.py
+++ fiba_reporter/player_stats_report.py
@@ -7,8 +7,8 @@ from fiba_inbounder.formulas import update_efg, update_usg, update_player_avg
 class FibaPlayerStatsReport():
     def _get_player_avg_df(self):
         player_stats_df = self.player_stats_df[self.player_stats_df['SECS'] > 0]
-        player_avg_stats_df = player_stats_df.groupby(['TeamCode', 'JerseyNumber', 'Name'], as_index=False, sort=False).mean()
-        player_avg_stats_df['GP'] = player_stats_df.groupby(['TeamCode', 'JerseyNumber', 'Name'], sort=False).size().reset_index(name='GP')['GP']
+        player_avg_stats_df = player_stats_df.groupby(['TeamCode', 'NumName'], as_index=False, sort=False).mean()
+        player_avg_stats_df['GP'] = player_stats_df.groupby(['TeamCode', 'NumName'], sort=False).size().reset_index(name='GP')['GP']
         update_player_avg(player_avg_stats_df)
 
         ps_df_list = list()
@@ -27,9 +27,10 @@ class FibaPlayerStatsReport():
         
         player_avg_stats_df = self._get_player_avg_df()
         for t in player_avg_stats_df['TeamCode'].unique():
-            header_str_list = '| # | Name | GP | Mins | 2PTM/A | 2PT% | 3PTM/A | 3PT% | FTM/A | FT% | OREB | DREB | REB | AST | STL | BLK | TOV | PF | PTS | eFG% |'
-            align_str_list = '|:---:|:---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|'
-            table_str = '|' + ps_df[['JerseyNumber', 'Name', 'GP', 'TP', 'FG2MA_STR', 'FG2P_STR', 'FG3MA_STR', 'FG3P_STR', 'FTMA_STR', 'FTP_STR',
+            ps_df = player_avg_stats_df[player_avg_stats_df['TeamCode'].str.match(t)]
+            header_str_list = '| Name | GP | Mins | 2PTM/A | 2PT% | 3PTM/A | 3PT% | FTM/A | FT% | OREB | DREB | REB | AST | STL | BLK | TOV | PF | PTS | eFG% |'
+            align_str_list = '|:---|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|'
+            table_str = '|' + ps_df[['NumName', 'GP', 'TP', 'FG2MA_STR', 'FG2P_STR', 'FG3MA_STR', 'FG3P_STR', 'FTMA_STR', 'FTP_STR',
             'OR', 'DR', 'REB', 'AS', 'ST', 'BS', 'TO', 'PF', 'PTS', 'EFG_STR']].to_csv(
                 sep='|',
                 line_terminator='|\n|',
@@ -44,7 +45,21 @@ class FibaPlayerStatsReport():
         return '\n'.join(result_str_list) + '\n'
 
     def _gen_player_avg_csv(self):
-        pass
+        result_str_list = list()
+        
+        player_avg_stats_df = self._get_player_avg_df()
+        for t in player_avg_stats_df['TeamCode'].unique():
+            ps_df = player_avg_stats_df[player_avg_stats_df['TeamCode'].str.match(t)]
+            table_str = ps_df[['NumName', 'GP', 'SECS', 'FG2P', 'FG2A', 'FG3P', 'FG3A', 'FTP', 'FTA',
+            'OR', 'DR', 'AS', 'ST', 'BS', 'TO', 'PF', 'PTS', 'USG', 'EFG']].to_csv(
+                float_format='%.1f',
+                encoding='utf-8',
+                index=False)[:]
+ 
+            t_result_str_list = [t, table_str]
+            result_str_list.append('\n'.join(t_result_str_list))
+
+        return '\n'.join(result_str_list) + '\n'
 
 class FibaPlayerStatsReportV5(FibaPlayerStatsReport):
     def __init__(self, game_id_list):
@@ -53,6 +68,7 @@ class FibaPlayerStatsReportV5(FibaPlayerStatsReport):
 
 def main():
     version = raw_input('fiba stats version?\n\t(5) v5\n\t(7) v7\n')
+    output_format = raw_input('Which format?\n\t(M)arkdown\n\t(C)SV\n')
     num_games = raw_input('How Many Games? ')
     game_id_list = list()
 
@@ -63,7 +79,10 @@ def main():
             game_id_list.append(str(match_id))
         r = FibaPlayerStatsReportV5(game_id_list)
 
-    print '## Player Stats\n' + r._gen_player_avg_md()
+    if str(output_format).lower() == 'm':
+        print '## Player Stats\n' + r._gen_player_avg_md()
+    elif str(output_format).lower() == 'c':
+        print r._gen_player_avg_csv()
 
 if __name__ == '__main__':
     main()
